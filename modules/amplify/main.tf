8362e776590f6eaa85d6e1efe0dd43f4ef79c210
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-15 23:32:32 +0530

    feat: update for auto build option

--- modules/amplify/main.tf
+++ modules/amplify/main.tf
@@ -2,6 +2,22 @@ resource "aws_amplify_app" "staff_attendance_app" {
   name        = var.app_name
   repository  = var.repository
   oauth_token = var.git_token
+
+  build_spec = <<-EOT
+    version: 1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - npm install  # Use npm install instead of npm ci
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+  EOT
 }
 
 resource "aws_amplify_branch" "staff_attendance_app_branch" {
