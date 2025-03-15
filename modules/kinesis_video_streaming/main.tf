6e0d312b46a98ddd58e9af5c54687dcd4a411517
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-15 20:43:10 +0530

    chor: disable kinesis

--- modules/kinesis_video_streaming/main.tf
+++ modules/kinesis_video_streaming/main.tf
@@ -1,6 +1,6 @@
-resource "aws_kinesis_video_signaling_channel" "webrtc_channel" {
+resource "aws_kinesis_video_stream" "webrtc_channel" {
   name = var.streaming_channel_name
-  type = "SINGLE_MASTER"
+  # type = "SINGLE_MASTER"
   tags = {
     Environment = "dev"
     Project     = "cloud staff attendance"
