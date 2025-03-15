455471a4a08bebac8b9e065dff60f0c25579e3f8
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-15 20:44:12 +0530

    chor: disable kinenis module

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "aws_kinesis_video_signaling_channel" {
-  source                 = "./modules/kinesis_video_streaming"
-  streaming_channel_name = "my-signaling-channel"
-}
+# module "aws_kinesis_video_signaling_channel" {
+#   source                 = "./modules/kinesis_video_streaming"
+#   streaming_channel_name = "my-signaling-channel"
+# }
