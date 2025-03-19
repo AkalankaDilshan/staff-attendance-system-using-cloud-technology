5b07c089714acc8d717fc829ace30cb55cbe6edb
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-20 00:21:15 +0530

    feat: create variables for lambda

---
+++ modules/registetion-lambda/variables.tf
@@ -0,0 +1,9 @@
+variable "function_name" {
+  description = "name for staff registetion lambda function"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "arn for lambda role"
+  type        = string
+}
